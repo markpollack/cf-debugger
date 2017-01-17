78fc932ef24acb326619acc622fdef884a83e7d4
Vinicius Carvalho <viniciusccarvalho@gmail.com>
2017-01-17 09:49:57 -0500

    Changing timeout

--- src/main/java/org/springframework/cloud/dataflow/CloudfoundryDebuggerApplication.java
+++ src/main/java/org/springframework/cloud/dataflow/CloudfoundryDebuggerApplication.java
@@ -3,6 +3,7 @@ package org.springframework.cloud.dataflow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.client.RestTemplate;
 
@@ -16,6 +17,10 @@ public class CloudfoundryDebuggerApplication {
 
 	@Bean
 	public RestTemplate client(){
-		return new RestTemplate();
+		HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+		httpRequestFactory.setReadTimeout(360*1000);
+		httpRequestFactory.setConnectTimeout(15*1000);
+		RestTemplate client = new RestTemplate(httpRequestFactory);
+		return client;
 	}
 }
